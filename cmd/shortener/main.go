cef6bab7315bef569a341285cf054fdb12406d4f
zavyalov <zavyalov@perco.ru>
2021-12-01 11:09:33 +0300

    testing

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,58 +11,61 @@ import (
 	"strings"
 )
 
-var urls = make(map[string]string)
+func handler(urls map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			data, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "invalid requestURL", http.StatusBadRequest)
+				break
+			}
+			url := string(data)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		data, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "invalid request", http.StatusBadRequest)
-			break
+			md5 := crypto.MD5.New()
+			md5.Write(data)
+			short := hex.EncodeToString(md5.Sum(nil))[:8]
+
+			urls[short] = url
+
+			w.WriteHeader(http.StatusCreated)
+			_, err = w.Write([]byte("http://localhost:8080/" + short))
+			if err != nil {
+				http.Error(w, "invalid requestURL", http.StatusBadRequest)
+				break
+			}
+		case http.MethodGet:
+			pattern := "^/?[0-9a-f]+/?$"
+			re := regexp.MustCompile(pattern)
+			url := r.URL.String()
+			match := re.Match([]byte(url))
+			if !match {
+				fmt.Println(match, url)
+				http.Error(w, "invalid request URL", http.StatusBadRequest)
+				break
+			}
+
+			shortURL := strings.Replace(re.FindStringSubmatch(url)[0], "/", "", -1)
+			longURL := urls[shortURL]
+			if longURL == "" {
+				http.NotFound(w, r)
+				//http.Error(w, "invalid requestURL", http.StatusBadRequest)
+				break
+			}
+
+			w.Header().Set("Location", longURL)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		default:
+			http.Error(w, "invalid requestURL", http.StatusBadRequest)
 		}
-		url := string(data)
-
-		md5 := crypto.MD5.New()
-		md5.Write(data)
-		short := hex.EncodeToString(md5.Sum(nil))[:8]
-
-		urls[short] = url
-
-		w.WriteHeader(http.StatusCreated)
-		_, err = w.Write([]byte("http://localhost:8080/" + short))
-		if err != nil {
-			http.Error(w, "invalid request", http.StatusBadRequest)
-			break
-		}
-	case http.MethodGet:
-		pattern := "^/?[0-9a-f]+/?$"
-		re := regexp.MustCompile(pattern)
-		url := r.URL.String()
-		match := re.Match([]byte(url))
-		if !match {
-			fmt.Println(match, url)
-			http.Error(w, "invalid request", http.StatusBadRequest)
-			break
-		}
-
-		shortURL := strings.Replace(re.FindStringSubmatch(url)[0], "/", "", -1)
-		longURL := urls[shortURL]
-		if longURL == "" {
-			//http.NotFound(w, r)
-			http.Error(w, "invalid request", http.StatusBadRequest)
-			break
-		}
-
-		w.Header().Set("Location", longURL)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	default:
-		http.Error(w, "invalid request", http.StatusBadRequest)
 	}
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	urls := make(map[string]string)
+
+	http.HandleFunc("/", handler(urls))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
