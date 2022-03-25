ffab225be83935ab019ef679ea421a1f93f5dc3c
zavyalov <zavyalov@perco.ru>
2022-03-25 08:37:03 +0300

    inc8 decode body

--- internal/middlewares/compress.go
+++ internal/middlewares/compress.go
@@ -32,65 +32,26 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 func GzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			gzr, err := gzip.NewReader(r.Body)
+			body, err := gzip.NewReader(r.Body)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			r.Body = gzr
-		}
-
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			r.Body = body
+		} else {
 			next.ServeHTTP(w, r)
 			return
 		}
+
 		gzw, err := gzip.NewWriterLevel(w, gzip.DefaultCompression)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		defer gzw.Close()
+
 		w.Header().Set("Content-Encoding", "gzip")
+
 		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gzw}, r)
-		//if !strings.Contains(strings.ToLower(r.Header.Get("Accept-Encoding")), "gzip") {
-		//	next.ServeHTTP(w, r)
-		//	return
-		//}
-		//
-		////// decode body
-		////body, err := gzip.NewReader(r.Body)
-		////if err != nil {
-		////	http.Error(w, err.Error(), http.StatusBadRequest)
-		////	return
-		////}
-		////r.Body = body
-		//
-		//gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		//if err != nil {
-		//	http.Error(w, err.Error(), http.StatusBadRequest)
-		//	return
-		//}
-		//defer gz.Close()
-		//
-		//w.Header().Set("Content-Encoding", "gzip")
-		//
-		//next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
-		//contentType := strings.ToLower(r.Header.Get("Content-Type"))
-		//
-		//var allowedCT bool
-		//
-		//// would love to replace it with golang.org/x/exp/slices.Contains()
-		//// but tests run on 1.17 :(
-		//for _, ct := range allowedTypes {
-		//	if ct == contentType {
-		//		allowedCT = true
-		//	}
-		//}
-		//
-		//if !allowedCT {
-		//	next.ServeHTTP(w, r)
-		//	return
-		//}
-		//
 	})
 }
